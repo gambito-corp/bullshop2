52b9ee3c71fe5a9f48eb9dca7b666340d597f292
Gambito-corp <asesor.pedro@gmail.com>
2022-11-06 21:25:46 +0100

    tercer commit implementando interface

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Facades\Voyager;
+
+class AuthController extends \TCG\Voyager\Http\Controllers\VoyagerAuthController
+{
+    public function login()
+    {
+        if ($this->guard()->user()) {
+            return redirect()->route('voyager.dashboard');
+        }
+        return Voyager::view('voyager::login');
+    }
+
+}
